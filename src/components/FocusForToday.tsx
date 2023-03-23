8983e751233d10713dc900cae844bdc0aea86531
borat <boram8655@gmail.com>
2023-03-23 09:44:19 -0300

    Revising the countdown timer on FocusForToday

--- src/components/FocusForToday.tsx
+++ src/components/FocusForToday.tsx
@@ -90,22 +90,16 @@ function FocusForToday() {
   const onChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
     const msgs = ["Nice!", "Great work!", "Good job!", "Way to go!"];
     const msg = msgs[Math.floor(Math.random() * msgs.length)];
-    let timeleft = 5;
-    if (event.target.checked) {
+    if (event.target.checked === true) {
       setStrikethrough(true);
-      const countdown = setInterval(function () {
-        if (timeleft <= 0) {
-          setCompliment("");
-          clearInterval(countdown);
-        } else {
-          setCompliment(msg);
-        }
-        timeleft -= 1;
-      }, 500);
+      setCompliment(msg);
+      setTimeout(function () {
+        setCompliment("");
+      }, 3000);
     }
-    if (!event.target.checked) {
-      setStrikethrough(false);
+    if (event.target.checked === false) {
       setCompliment("");
+      setStrikethrough(false);
     }
   };
 
@@ -135,70 +129,6 @@ function FocusForToday() {
       )}
     </div>
   );
-  // const [loading, setLoading] = useState(false);
-  // const [focus, setFocus] = useState("");
-  // const [strikethrough, setStrikethrough] = useState(false);
-  // const [compliment, setCompliment] = useState("");
-  // const [isMouseOver, setIsMouseOver] = useState(false);
-  // const [isChecked, setIsChecked] = useState(false);
-
-  // const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-  //   setFocus(event.currentTarget.value);
-  // };
-  // const showFocus = (event: React.KeyboardEvent<HTMLInputElement>) => {
-  //   if (event.key === "Enter" && focus !== "") {
-  //     setLoading(true);
-  //   }
-  // };
-  // const onChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const msg = ["Nice!", "Great work!", "Good job!", "Way to go!"];
-  //   setIsChecked(event.target.checked);
-  //   if (event.target.checked) {
-  //     setStrikethrough(true);
-  //     setCompliment(msg[Math.floor(Math.random() * msg.length)]);
-  //   }
-  //   if (!event.target.checked) {
-  //     setStrikethrough(false);
-  //     setCompliment("");
-  //   }
-  // };
-  // const onMouseover = () => {
-  //   setIsMouseOver(true);
-  // };
-  // const onMouseout = () => {
-  //   setIsMouseOver(false);
-  // };
-
-  // return (
-  //   <div style={{ display: "flex", justifyContent: "center" }}>
-  //     {loading ? (
-  //       <FocusWCheckBox>
-  // <h1>TODAY</h1>
-  // <TodaysFocus onMouseOver={onMouseover} onMouseOut={onMouseout}>
-  //   <label>
-  //     {isMouseOver ? (
-  //       <input onChange={onChecked} type="checkbox" />
-  //     ) : null}
-
-  //     <h1 style={{ fontSize: 45 }}>{focus}</h1>
-  //   </label>
-  //   {isMouseOver ? <div>💬</div> : null}
-  // </TodaysFocus>
-  // <span>{strikethrough ? <h1>{compliment}</h1> : null}</span>
-  //       </FocusWCheckBox>
-  //     ) : (
-  //       <AskFocus>
-  //         <h1>What is your main focus today?</h1>
-  //         <input
-  //           value={focus}
-  //           onChange={onChange}
-  //           onKeyDown={showFocus}
-  //           type="text"
-  //         />
-  //       </AskFocus>
-  //     )}
-  //   </div>
-  // );
 }
 
 export default FocusForToday;
